d946be2f8e768e8de92c30c107c19990685e1ab6
Stanislas Lejay <stanislas.lejay@epita.fr>
2016-05-14 10:47:26 +0200

    Update tests
    Signed-off-by: Stanislas Lejay <stanislas.lejay@epita.fr>

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-for file in $VPD_DIR/*linux*.bin; do
-        echo -e "\n$file"
-        time volatility -f $file linux_get_profile -v
-done
-
 for file in $VPD_DIR/*.bin; do
         echo -e "\n$file"
         time volatility -f $file profilescan -v
